3e5f102eb4e211704c37bbdc1d5d61abca35aba4
Ram Poptani <ram.poptani.work@gmail.com>
2026-02-20 12:00:57 +0530

    fix: use static date in sitemap to avoid caching issues

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,7 +4,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: "https://ram-poptani.vercel.app",
-      lastModified: new Date(),
+      lastModified: new Date("2026-02-20"),
       changeFrequency: "weekly",
       priority: 1,
     },
